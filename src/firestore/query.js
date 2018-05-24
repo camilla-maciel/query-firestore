61bbe18e6d0ee4acafdadf25ebb03b9742177e41
camillamaciel <camilla.leicam@yahoo.com>
2018-05-24 01:54:13 -0300

    Firebase setup and firestore queries

---
+++ src/firestore/query.js
@@ -0,0 +1,25 @@
+import 'firebase/firestore';
+import getInstance from './setup';
+
+import { TYPES } from '../constants';
+
+const parseClauseValue = (clause) => {
+  switch (clause.type) {
+    case TYPES.NUMBER:
+      return Number.parseFloat(clause.value);
+    case TYPES.BOOLEAN:
+      return clause.value === 'true';
+    case TYPES.TIMESTAMP:
+      return new Date(clause.value);
+    default:
+      return clause.value;
+  }
+};
+
+export const query = (collection, clauses) => {
+  let collectionRef = getInstance().collection(collection);
+  clauses.forEach((clause) => {
+    collectionRef = collectionRef.where(clause.field, clause.comparison, parseClauseValue(clause));
+  });
+  return collectionRef.get();
+};
---
+++ src/firestore/setup.js
@@ -0,0 +1,23 @@
+import 'firebase/firestore';
+import * as firebase from 'firebase/app';
+
+let loaded;
+let firestore;
+
+const getInstance = (firebaseKeys) => {
+  if (loaded) {
+    return firestore;
+  }
+  
+  firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  });
+
+  firestore = firebase.firestore();
+  loaded = true;
+
+  return firestore;
+}
+
+export default getInstance;
